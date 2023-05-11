84af039d1c43df20f14aa29f61563d8fba9a3df4
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-05-11 16:23:55 -0400

    Setup ES Lint and Prettier

---
+++ gutenburg-blocks/boilerplate/.eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@wordpress/eslint-plugin/recommended-with-formatting',
+		'prettier',
+	],
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	plugins: ['react'],
+	rules: {},
+};
