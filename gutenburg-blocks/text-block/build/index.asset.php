9c58b7ffbc895662f5ae4868fb236a948516979c
Adam Januszkiewicz <ajanuszkiewicz@gmail.com>
2023-05-16 23:07:25 -0400

    Adding controls to the block toolbar

--- gutenburg-blocks/text-block/build/index.asset.php
+++ gutenburg-blocks/text-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'f5e6b814e1de75768b5b381e22df3b7f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'b80b4608df334c1675382e47dc4a1e66');
--- gutenburg-blocks/text-block/src/edit.js
+++ gutenburg-blocks/text-block/src/edit.js
@@ -1,11 +1,88 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import {
+	useBlockProps,
+	RichText,
+	BlockControls,
+} from '@wordpress/block-editor';
+import {
+	ToolbarGroup,
+	ToolbarButton,
+	ToolbarDropdownMenu,
+} from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { text } = attributes;
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
+		<>
+			<BlockControls group="inline">
+				<p>Inline Controls</p>
+			</BlockControls>
+			<BlockControls group="block">
+				<p>Block Controls</p>
+			</BlockControls>
+			<BlockControls
+				group="other"
+				controls={[
+					{
+						title: 'Button 1',
+						icon: 'admin-generic',
+						isActive: true,
+						onClick: () => console.log('Button 1 Clicked'),
+					},
+					{
+						title: 'Button 2',
+						icon: 'admin-collapse',
+						isActive: false,
+						onClick: () => console.log('Button 2 Clicked'),
+					},
+				]}
+			>
+				{text && (
+					<ToolbarGroup>
+						<ToolbarButton
+							title="Align Left"
+							icon="editor-alignleft"
+							onClick={() => console.log('Align Left')}
+						/>
+						<ToolbarButton
+							title="Align Center"
+							icon="editor-aligncenter"
+							onClick={() => console.log('Align Center')}
+						/>
+						<ToolbarButton
+							title="Align Right"
+							icon="editor-alignright"
+							onClick={() => console.log('Align Right')}
+						/>
+						<ToolbarDropdownMenu
+							icon="arrow-down"
+							label={__('More Alignment Options', 'text-box')}
+							controls={[
+								{
+									title: __('Wide', 'text-box'),
+									icon: 'align-wide',
+								},
+								{
+									title: __('Full', 'text-box'),
+									icon: 'align-full-width',
+								},
+							]}
+						/>
+					</ToolbarGroup>
+				)}
+				<ToolbarGroup>
+					<p>Test</p>
+				</ToolbarGroup>
+			</BlockControls>
+			<RichText
+				{...useBlockProps()}
+				onChange={(value) => setAttributes({ text: value })}
+				value={text}
+				placeholder={__('Your text')}
+				tagName="h4"
+				allowedFormats={[]}
+			/>
+		</>
 	);
 }
